91c660a26e5553e945431214e0c00d4c639f5123
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-12 18:17:04 +0900

    수정

--- sql02/sql02-1.sql
+++ sql02/sql02-1.sql
@@ -1,4 +1,7 @@
 --1번문제 
+/*
+ * 각 사원에 대해서 사번, 이름, 부서명, 매니저의 이름을 조회하세요
+ */
  select em.employee_id, 
         em.first_name, 
         de.department_name, 
@@ -7,11 +10,19 @@
  where em.manager_id = de.manager_id;
  
 --2번문제
+/*
+ * 지역에 속한 나라들을 지역이름, 나라이름으로 출력하되 지역이름, 나라이름 순서대로 내림차순으로 정렬하세요
+ */
 select re.region_name, co.country_name
 from countries co, regions re 
 where co.region_id = re.region_id
 order by re.region_name desc;
+
 --3번문제
+/*
+ * 각 부서에 대해서 부서번호, 부서이름, 매니저의 이름, 
+ * 위치한 도시, 나라의 이름 그리고 지역구분의 이름까지 전부 출력하세요
+ */
 select de.department_id, 
        de.department_name, 
        man.first_name, 
@@ -29,6 +40,10 @@ where man.employee_id = de.manager_id
       and co.region_id = re.region_id; 
       
 --4번문제
+/*
+ * public Accountant의 직책으로 과거에 근무한적이 있는 모든 사원의 사번과
+ * 이름을 출력하세요. 이름은 first_name과 last_name을 합쳐 출력합니다.
+ */
 select  em.employee_id, 
         em.first_name || em.last_name 
 from job_history jo , jobs j ,employees em
@@ -37,6 +52,10 @@ where jo.job_id=j.job_id
       and em.employee_id = jo.employee_id;
       
 --5번 문제
+/*
+ * 직원들의 사번, 이름, 성과 부서이름을 조회하여
+ * 성(last_name)순서로 오름차순 정렬하세요
+ */
 select em.employee_id, 
        em.first_name, 
        em.last_name, 
@@ -47,6 +66,9 @@ where em.department_id = de.department_id
 order by last_name asc;
 
 --6번문제
+/*
+ * 자신의 매니저보다 채용일이 빠른 사원의 사번, 성, 채용일을 조회하세요
+ */
 select em.employee_id, 
 	   em.last_name, 
 	   em.hire_date, 
