3e29e88e44ae359350beacc21fcd0b85b4807d28
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-15 13:49:25 +0900

    1

---
+++ sql02/sql03.sql
@@ -0,0 +1,41 @@
+--1번문제
+
+select count(*)
+from employees
+where  salary <all(select avg(salary) from employees);
+
+--2번문제
+
+select employee_id, 
+	   last_name, 
+	   salary, 
+	   department_id
+from employees
+where (department_id,salary) in(select department_id,
+                                max(salary) 
+                                from employees 
+                                group by department_id)
+order by salary desc;
+
+--3번문제 아직 틀림
+
+select j.job_title, 
+       salary
+from jobs j, (select job_id, 
+                     sum(salary)salary 
+              from employees 
+              group by job_id)s 
+where j.job_id = s.job_id
+order by salary desc;
+
+--4번문제
+
+select e.employee_id, 
+       e.last_name, 
+       e.salary
+from employees e, (select department_id, 
+                          avg(salary)average
+                   from employees 
+                   group by department_id)s
+where e.department_id = s.department_id 
+      and s.average < e.salary;
